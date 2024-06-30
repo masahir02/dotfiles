fef43b5233cd47e24d1a43aa2c5af60db5ad0531
masahir02 <masahir02@users.noreply.github.com>
2024-07-01 08:18:15 +0900

    2024-07-01 08:18:15

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -8,16 +8,6 @@ require('mason').setup({
   }
 })
 
-require("mason-lspconfig").setup {
-  ensure_installed = {
-    'gopls',
-    'pyright',
-    'tsserver',
-    'volar',
-  },
-  automatic_installation = true,
-}
-
 require("mason-lspconfig").setup_handlers {
   function (server_name)
     require('lspconfig')[server_name].setup({
