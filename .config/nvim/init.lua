3a61170760d0ed93ca7ff6c0648927a33820a916
masahir02 <masahir02@users.noreply.github.com>
2025-04-23 19:57:53 +0900

    2025-04-23 19:57:53

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -139,8 +139,64 @@ require('lazy').setup({
       'hrsh7th/nvim-cmp',
       dependencies = {
         'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'saadparwaiz1/cmp_luasnip',
         'L3MON4D3/LuaSnip',
-      }
+        'rafamadriz/friendly-snippets',
+        'zbirenbaum/copilot-cmp',
+        'zbirenbaum/copilot.lua',
+      },
+      config = function()
+        require("copilot").setup({
+          suggestion = { enabled = false },
+          panel = { enabled = false },
+        })
+        require("copilot_cmp").setup()
+
+        local cmp = require('cmp')
+        local luasnip = require('luasnip')
+
+        require("luasnip.loaders.from_vscode").lazy_load()
+
+        cmp.setup({
+          snippet = {
+            expand = function(args)
+              luasnip.lsp_expand(args.body)
+            end,
+          },
+          mapping = cmp.mapping.preset.insert({
+            ['<Tab>'] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+              else
+                fallback()
+              end
+            end, { "i", "s" }),
+            ['<S-Tab>'] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { "i", "s" }),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            ['<C-Space>'] = cmp.mapping.complete(),
+          }),
+          sources = cmp.config.sources({
+            { name = 'copilot' },
+            { name = 'nvim_lsp' },
+            { name = 'luasnip' },
+          }, {
+            { name = 'buffer' },
+            { name = 'path' },
+          }),
+        })
+      end
     },
 
     {
@@ -173,24 +229,6 @@ require('lazy').setup({
             })
           end,
         }
-
-        local cmp = require('cmp')
-        cmp.setup({
-          snippet = {
-            expand = function(args)
-              require('luasnip').lsp_expand(args.body)
-            end,
-          },
-          mapping = cmp.mapping.preset.insert({
-            ['<Tab>'] = cmp.mapping.select_next_item(),
-            ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          }),
-          sources = {
-            { name = 'nvim_lsp' },
-            { name = 'luasnip' },
-          },
-        })
       end,
     },
 
